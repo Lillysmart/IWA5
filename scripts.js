ba20a4a3ffd71aff590ddc08143bc09d26dbf939
Lillysmart <137174363+Lillysmart@users.noreply.github.com>
2023-08-07 12:15:35 +0200

    Add condition after && in L30

--- scripts.js
+++ scripts.js
@@ -5,7 +5,8 @@ NONE_SELECTED = '0'
 
 customers = '1'
 let location = 'RSA'
- let currency = "R"
+ let currency = R
+ shipping=null
 
 
 if (location === "RSA") { console.log(shipping = 400 ,currency) }
@@ -17,16 +18,16 @@ if (location === "NAM"){
     console.log(shipping = 600 , currency) }
 else {shipping = 800 , currency}
 
-let shoes = 300 * 1
+ let shoes = 300 * 1
 toys =100 * 5
 shirts = 150 * 'NONE_SELECTED'
 batteries = 35 * 2
 pens = 5 * 'NONE_SELECTED' 
 
 shipping = null
-currency = $
+currency = 'R'
 
-if (shoes + batteries + pens + shirts > 1000 &&  ) {
+if (shoes + batteries + pens + shirts > 1000 , Currecny && location==='RSA'|| location==='NAM'  ) {
 	if (location = NAM && customers < 2) {
 			if (location = RSA)
 		    shipping = 0 || calcShipping
