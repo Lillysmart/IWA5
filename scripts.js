75c2f00ceff0d0f6d8209cb84852ae79e1abaf38
Lillysmart <137174363+Lillysmart@users.noreply.github.com>
2023-08-07 11:59:50 +0200

    add location=NAM

--- scripts.js
+++ scripts.js
@@ -1,13 +1,20 @@
 
 FREE_WARNING = 'Free shipping only applies to single customer orders'
 BANNED_WARNIN = 'Unfortunately we do not ship to your country of residence'
-NONE_SELECTED = '0'
+NONE_SELECTED = '0'  
 
-if (location = RSA) { shipping === 400 && currency === 'R' }
+customers = '1'
+let location = 'RSA'
+currency = null
 
-if location = NAM
-shipping = 600 
-else shipping = 800
+
+if (location === "RSA") { console.log(shipping === 400 && currency === 'R') }
+
+
+ location= 'NAM'
+if (location === "NAM"){
+    shipping = 600 }
+else {shipping = 800}
 
 shoes = 300 * 1
 toys - 100 * 5
@@ -30,6 +37,3 @@ if (shipping = 0) && (customers !=== 1) { console.log(WARNING) }
 
 location = 'NK' ? console.log(WARNING) : console.log('price', currency, shoes + batteries + pens + shirts + shipping)
 
-customers = '1'
-const location = 'RSA'
-currency = null
