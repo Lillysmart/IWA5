494d3a0cd2dd86597396de3da3587a52210f6c5d
Lillysmart <137174363+Lillysmart@users.noreply.github.com>
2023-08-08 10:58:05 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,41 +1,39 @@
 
-FREE_WARNING = 'Free shipping only applies to single customer orders'
-BANNED_WARNIN = 'Unfortunately we do not ship to your country of residence'
-NONE_SELECTED = '0'  
+ const FREE_WARNING = 'Free shipping only applies to single customer orders';
+ const BANNED_WARNING= 'Unfortunately we do not ship to your country of residence'
+ const NONE_SELECTED = 0  
 
-customers = '1'
+let customers = 1
 let location = 'RSA'
- let currency = R
- shipping=null
+let currency = null
+ let shipping=null
 
 
-if (location === "RSA") { console.log(shipping = 400 ,currency) }
+if (location === "RSA") { console.log( currency='R',shipping = 400 ) }
 
-
- location= 'NAM' 
+location= 'NAM' 
 currency= "$"
 if (location === "NAM"){ 
-    console.log(shipping = 600 , currency) }
-else {shipping = 800 , currency}
+    console.log(currency,shipping = 600) }
+else { console.log(shipping = 800 , currency)}
 
- let shoes = 300 * 1
-toys =100 * 5
-shirts = 150 * 'NONE_SELECTED'
-batteries = 35 * 2
-pens = 5 * 'NONE_SELECTED' 
-
-shipping = null
+ const shoes = 300 *1
+const toys =100 * 5
+const shirts = 150 * NONE_SELECTED
+const batteries = 35 * 2
+const pens = 5 * NONE_SELECTED 
+let cost =shoes+ toys+ shirts+ batteries+ pens ; 
+ 
 currency = 'R'
+let warning= "WARNING!"
+let free_shipping="FREE SHIPPING"
 
-if (shoes + batteries + pens + shirts > 1000 , Currecny && location==='RSA'|| location==='NAM'  ) {
-	if (location = NAM && customers < 2) {
-			if (location = RSA)
-		    shipping = 0 || calcShipping
-		}
-	}
-}
+if (cost >1000 && location==='RSA'
+  || location==='NAM' && customers === 1  ) { console.log (free_shipping);}
 
-if (shipping = 0) && (customers !=== 1) { console.log(WARNING) }
-
-location = 'NK' ? console.log(WARNING) : console.log('price', currency, shoes + batteries + pens + shirts + shipping)
-
+  if (cost > 1000 &&  location !== 'NAM' || location !=='RSA' && customers !== 1) 
+  {console.log (FREE_WARNING)}
+		
+location="NK"
+location === 'NK' ? console.log(BANNED_WARNING): console.log('price', currency, shoes + batteries + pens + shirts +shipping )
+console.log('price'+ currency + (cost +shipping) )
